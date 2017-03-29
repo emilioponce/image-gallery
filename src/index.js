c338cc7351c57083490d7834c4709770d46af28a
Emilio <yosoycore@gmail.com>
2017-03-29 19:20:14 +0200

    freeze middleware added in order to prevent mutable state

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { createStore, applyMiddleware } from 'redux';
 import RootReducer from './reducers/root-reducer';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import freeze from 'redux-freeze';
 
 import App from './App';
 
 const logger = createLogger();
-const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
+
+//@TODO logger and freeze middlewares only in DEV environment
+const createStoreWithMiddleware = applyMiddleware(thunk, freeze, logger)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(RootReducer)}>
